5768eee50517dc09e7e517a11fbbf9cc6fda7137
Martin <martin.dinahet@gmail.com>
2026-09-07 20:56:20 +0200

    feature(services/todos): add todo CRUD API with Prisma

---
+++ services/todos/src/app.ts
@@ -0,0 +1,47 @@
+import { Hono } from "hono";
+import { prisma } from "./prisma.js";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { HTTPException } from "hono/http-exception";
+
+const CreateTodoSchema = z.object({
+  title: z.string().min(1),
+  completed: z.boolean().optional().default(false),
+});
+
+const UpdateTodoSchema = z.object({
+  title: z.string().min(1).optional(),
+  completed: z.boolean().optional(),
+});
+
+export const app = new Hono() //
+  .get("/todos", async (c) => {
+    const todos = await prisma.todo.findMany();
+    if (!todos) throw new HTTPException(500, { message: "Internal server error" });
+    return c.json({ todos }, 200);
+  })
+  .post("/todos", zValidator("json", CreateTodoSchema), async (c) => {
+    const data = c.req.valid("json");
+    const todo = await prisma.todo.create({ data });
+    if (!todo) throw new HTTPException(500, { message: "Internal server error" });
+    return c.json({ todo }, 201);
+  })
+  .get("/todos/:id", async (c) => {
+    const id = c.req.param("id");
+    const todo = await prisma.todo.findUnique({ where: { id } });
+    if (!todo) throw new HTTPException(500, { message: "Internal server error" });
+    return c.json({ todo }, 200);
+  })
+  .put("/todos/:id", zValidator("json", UpdateTodoSchema), async (c) => {
+    const id = c.req.param("id");
+    const data = c.req.valid("json");
+    const todo = await prisma.todo.update({ where: { id }, data });
+    if (!todo) throw new HTTPException(500, { message: "Internal server error" });
+    return c.json({ todo }, 200);
+  })
+  .delete("/todos/:id", async (c) => {
+    const id = c.req.param("id");
+    const todo = await prisma.todo.delete({ where: { id } });
+    if (!todo) throw new HTTPException(500, { message: "Internal server error" });
+    return c.json({ todo }, 200);
+  });
